8eebb9ddc2404f66ad506023c425336df96c3fb5
sarwin-github <dapito.sherwin@yahoo.com>
2018-05-30 18:18:42 +0800

    fixing error

--- src/app/api/api-host-config/host-config.ts
+++ src/app/api/api-host-config/host-config.ts
@@ -1,4 +1,4 @@
-const hostUrl = ''; //heroku host url
+const hostUrl = 'https://mean-app-crud.herokuapp.com/'; //heroku host url
 //https://entrenami-frontend.herokuapp.com
 //https://entrenami-api.herokuapp.com
 export { hostUrl };
